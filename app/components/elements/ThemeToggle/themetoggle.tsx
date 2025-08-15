e11cc60dc3736ccbe1a10e72e3310bba6db9d1e8
Zain <abibz97@gmail.com>
2025-08-15 14:00:58 +0700

    modified css

---
+++ app/components/elements/ThemeToggle/themetoggle.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import { Moon, Sun } from "lucide-react";
+import { useState } from "react";
+
+const ThemeToggle = () => {
+  const [isDarkMode, setIsDarkMode] = useState(false);
+  const toggleTheme = () => {
+    if (isDarkMode) {
+      document.documentElement.classList.remove("dark");
+      localStorage.setItem("theme", "light");
+      setIsDarkMode(false);
+    } else {
+      document.documentElement.classList.add("dark");
+      localStorage.setItem("theme", "dark");
+      setIsDarkMode(true);
+    }
+  };
+
+  return (
+    <button onClick={toggleTheme}>
+      {isDarkMode ? (
+        <Sun className="h-6 w-6 text-yellow-300" />
+      ) : (
+        <Moon className="h-6 w-6 text-blue-900" />
+      )}
+    </button>
+  );
+};
+
+export default ThemeToggle;
--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,26 @@
-import Background from "./components/layout/background";
 import Hero from "./components/layout/hero";
 import NavBar from "./components/layout/navbar";
 import Experience from "./components/layout/experience";
 import Project from "./components/layout/project";
+import ThemeToggle from "./components/elements/ThemeToggle/themetoggle";
 
 export default function Home() {
   return (
-    <div className="w-full min-h-screen overflow-x-hidden bg-[#e4e6eb] font-jetbrains gap-10 text-slate-700 flex flex-col">
-      {/* <Background /> */}
+    <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
+      {/* Theme Toggle */}
+      <ThemeToggle/>
+
+      {/* Background  Effect */}
+
+      {/* Navbar */}
       <NavBar />
+
+      {/* Main Content*/}
       <Hero />
-      <div className="w-full flex items-start justify-start bg-[#e4e6eb] gap-10">
-        <Experience />
-        <Project/>
-      </div>
+      <Project />
+
+      {/* Footer */}
+
     </div>
   );
 }
